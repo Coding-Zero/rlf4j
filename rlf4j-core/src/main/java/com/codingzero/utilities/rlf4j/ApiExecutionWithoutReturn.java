492d15e82cd110778853337d4c1e4fc31417091b
ruisun <stevesuncanada@gmail.com>
2020-11-30 18:34:21 +0800

    - Remove RateLimitRuleRegister - Make whole RateLimiter generic for api instance.

---
+++ rlf4j-core/src/main/java/com/codingzero/utilities/rlf4j/ApiExecutionWithoutReturn.java
@@ -0,0 +1,11 @@
+package com.codingzero.utilities.rlf4j;
+
+/**
+ * This interface represents a no return value API method reference
+ */
+@FunctionalInterface
+public interface ApiExecutionWithoutReturn {
+
+    void execute();
+
+}
